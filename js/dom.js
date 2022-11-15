5623cd3de08631b4f94ebcd0aa1dea9456aa68d1
webriaz176 <mohammadriaz2206@gmail.com>
2022-11-15 18:58:14 +0600

    event added

---
+++ js/dom.js
@@ -0,0 +1,5 @@
+const places= document.querySelectorAll('.first-1 li');
+for(const place of places){
+    // console.log(place.innerText);
+}
+
---
+++ js/event.js
@@ -0,0 +1,15 @@
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+}
+
+const makeRed = document.getElementById('make-red');
+makeRed.onclick=makeRedd;
+
+function makeRedd(){
+    document.body.style.backgroundColor = 'red';
+}
+
+const makeYellow = document.getElementById('make-yellow');
+makeYellow.onclick= function(){
+    document.body.style.backgroundColor = 'yellow';
+}
