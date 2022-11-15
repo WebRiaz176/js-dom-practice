4d653df9e7b295c89b6dbcd1e0ba8289ec1ab469
webriaz176 <mohammadriaz2206@gmail.com>
2022-11-15 22:32:45 +0600

    delete-js

---
+++ js/github-delete.js
@@ -0,0 +1,18 @@
+
+
+
+document.getElementById('btn-box').addEventListener('click', function(){
+    const secret = document.getElementById('heading-text');
+    secret.style.display='none';
+})
+
+document.getElementById('input-text').addEventListener('keyup', function(event){
+    const text = event.target.value;
+    const deleteButton = document.getElementById('btn-box');
+    if(text === 'delete'){
+        deleteButton.removeAttribute('disabled');
+    }
+    else{
+        deleteButton.setAttribute('disabled', true);
+    }
+})
